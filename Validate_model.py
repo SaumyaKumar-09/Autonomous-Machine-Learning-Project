e2b2d2f5bb9d29f57b5963205a239e785cf15ab5
Saumya Kumar <131158488+SaumyaKumar-09@users.noreply.github.com>
2024-10-21 21:24:11 +0530

    Update Validate_model.py

--- Validate_model.py
+++ Validate_model.py
@@ -3,10 +3,10 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 
 # Load the serialized model
-model = joblib.load(r'E:\Extion Infotech Internship\Task 1\best_model.joblib')
+model = joblib.load(r'PATH\TO\best_model.joblib')
 
 # Load the dataset for testing
-test_data = pd.read_csv(r'E:\Extion Infotech Internship\Task 1\iris_test.csv')
+test_data = pd.read_csv(r'PATH\TO\iris_test.csv')
 X_test = test_data.drop('target', axis=1)
 y_test = test_data['target']
 
